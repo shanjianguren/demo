73655378011fa70002b46982c381ad42a58775e7
shanjianguren <1234qwer>
2019-12-04 22:41:51 +0800

    gupiao

---
+++ demo-concurrent/src/main/java/demo/Gupiao.java
@@ -0,0 +1,13 @@
+package demo;
+
+import java.util.Arrays;
+
+public class Gupiao {
+    public static void main(String[] args) {
+        System.out.println("args = https://bbs.pinggu.org/thread-5022702-1-1.html" +
+                "" +
+                "https://www.liangzl.com/get-article-detail-14585.html" +
+                "https://www.cnblogs.com/ibearpig/p/3647440.html" +
+                "https://bbs.pinggu.org/thread-5022702-1-1.html" + Arrays.deepToString(args));
+    }
+}
